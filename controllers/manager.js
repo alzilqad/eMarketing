6a903a0b4572543ed4bf8ed9e10e4fdd6632b334
ki-shohag <koushikur.aiub@gmail.com>
2020-11-09 17:06:39 +0600

    Added login module, manager profile crud

--- controllers/manager.js
+++ controllers/manager.js
@@ -3,4 +3,16 @@ const router = express.Router();
 router.get('/', (req, res)=>{
     res.render('manager/home/index');
 });
+router.get('/chat', (req, res)=>{
+    res.render('manager/chat/index');
+});
+router.get('/profile', (req, res)=>{
+    res.render('manager/profile/index');
+});
+router.get('/profile/edit/:id', (req, res)=>{
+    res.render('manager/profile/edit');
+});
+router.get('/login', (req, res)=>{
+    res.render('manager/home/login');
+});
 module.exports = router;
